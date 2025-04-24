92299d8aeebf56710b0b6cd2826c2a73ce5656e3
Davide Ghilardi <ghilardi.davide@gmail.com>
2025-04-24 19:33:07 +0200

    add login page

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,14 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { LoginPage } from './page/LoginPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <LoginPage />
+      {/*<App />*/}
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
