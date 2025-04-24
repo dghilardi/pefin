98c0571f3abc2260d29cde54f70293cee0129808
Davide Ghilardi <ghilardi.davide@gmail.com>
2025-04-24 11:12:36 +0200

    reinitialize as react project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
