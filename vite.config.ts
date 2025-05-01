671ac639513e288db1f18a275c874522ee142ddc
Davide Ghilardi <ghilardi.davide@gmail.com>
2025-05-01 23:30:21 +0200

    reverse proxy api

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
